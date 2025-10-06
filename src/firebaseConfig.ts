3a56159226988de1f2c9892b3ea23d3d1ad093f4
stu77 <stevenwts.sw@gmail.com>
2025-10-06 11:17:21 +0800

    update config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDf1AE5kW7lB-4nDwM8H4pdi1VxjjL4Llw",
-  authDomain: "petrecord-84cb4.firebaseapp.com",
-  databaseURL: "https://petrecord-84cb4-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "petrecord-84cb4",
-  storageBucket: "petrecord-84cb4.firebasestorage.app",
-  messagingSenderId: "59598373859",
-  appId: "1:59598373859:web:a77d5469de457ffaedd6d6",
-  measurementId: "G-5SX9Z27G44"
+ apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
